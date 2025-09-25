7888b33e250c287652bff7189eddf1557414add9
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-09-25 22:50:48 +0300

    add card.module.css

--- client/src/components/cards/Card.jsx
+++ client/src/components/cards/Card.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./cards.module.css";
 import BellIcon from "../../assets/icons/bell-light.svg";
 import ArrowIcon from "../../assets/icons/arrow.svg";
 import EditIcon from "../../assets/icons/pencil-01.svg";
@@ -7,40 +8,44 @@ import TrashIcon from "../../assets/icons/trash-04.svg";
 const Card = ({ card, onEdit, onDelete }) => {
   const { title, description, priority, deadline } = card;
 
-  // Bugünün tarihi
   const today = new Date().toISOString().split("T")[0];
   const isDeadlineToday = deadline === today;
 
   return (
-    <div className="card">
-      <div className="card-header">
+    <div className={styles.card}>
+      <div className={styles["card-header"]}>
         <h3>{title}</h3>
-
-        <div className="card-actions">
+        <div className={styles["card-actions"]}>
           {isDeadlineToday && (
-            <button className="icon-btn">
+            <button className={styles["icon-btn"]}>
               <img src={BellIcon} alt="Deadline Today" />
             </button>
           )}
-          <button className="icon-btn">
+          <button className={styles["icon-btn"]}>
             <img src={ArrowIcon} alt="Move" />
           </button>
-
-          <button className="icon-btn" onClick={() => onEdit(card)}>
+          <button
+            className={styles["icon-btn"]}
+            onClick={() => onEdit(card)}
+          >
             <img src={EditIcon} alt="Edit" />
           </button>
-
-          <button className="icon-btn" onClick={() => onDelete(card.id)}>
+          <button
+            className={styles["icon-btn"]}
+            onClick={() => onDelete(card.id)}
+          >
             <img src={TrashIcon} alt="Delete" />
           </button>
         </div>
       </div>
 
-      <p className="card-description">{description}</p>
+      <p className={styles["card-description"]}>{description}</p>
 
-      <div className="card-footer">
-        <span className={`priority ${priority}`}>{priority}</span>
-        <span className="deadline">{deadline}</span>
+      <div className={styles["card-footer"]}>
+        <span className={`${styles.priority} ${styles[priority]}`}>
+          {priority}
+        </span>
+        <span className={styles.deadline}>{deadline}</span>
       </div>
     </div>
   );
