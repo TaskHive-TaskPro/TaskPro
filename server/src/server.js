02151c63948648b231de4b9d26c51f8aa9e4f08d
Habibe Çınar <146085701+habibecinar@users.noreply.github.com>
2025-09-29 22:14:56 +0300

    register solved

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'http://localhost:3001',
     'http://127.0.0.1:3000',
+    'http://127.0.0.1:3001',
     process.env.CLIENT_URL
   ],
   credentials: true,
