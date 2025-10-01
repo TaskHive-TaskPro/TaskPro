7111a9b6fb052aa17e04779c8021d09ed92c685f
Emre Yiğit <emreygt01@icloud.com>
2025-10-01 14:47:22 +0300

    import added

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import cardRoutes from './routes/cards.js';
 import authRoutes from './routes/authRoutes.js';
+import boards from "./routes/boards.js";
 
 
 dotenv.config();
@@ -39,7 +40,7 @@ connectDB();
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/cards', cardRoutes);
-app.use("/api/boards", boardsRoutes);
+app.use("/api/boards", boards);
 
 // Test route
 app.get('/', (req, res) => res.send('Server çalışıyor'));
